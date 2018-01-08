82d2d763422dfa9a4c3401f68283eea143e78795
yolangelica <rayman.yolanda@gmail.com>
2018-01-08 18:35:55 -0300

    ETAPA7 imprime texto en recipehtml

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,11 @@
 $(document).ready( function(){
 $(".js-menu").hide();/*hide oculta icono menú en pag recipe*/
 
+
+     //evento - DOM listo (Dom Ready)
+     $(document).ready(function(){
+       console.log('Load Page')
+     });
+
+
 });
